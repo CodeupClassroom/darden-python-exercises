0af390cdb0f39ea58d05eb1dabecc8ea0f6f20f8
Zach Gulde <zachgulde@gmail.com>
2020-08-06 14:59:09 -0500

    Add importing example

--- import_demo.py
+++ import_demo.py
@@ -1,6 +1,22 @@
-def sayhello():
-    return "Hello, Darden!"
+def a():
+    return 1
 
-food_for_lunch = 'Triscuits & Hummus'
+def b():
+    return 2
 
-print('Hi There!')
+# If I import c in another file or notebook, I don't also have to import a and b
+def c():
+    return a() + b()
+
+
+def acquire_data():
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def prepare_data(data):
+    return [str(x) for x in data if x < 8]
+# If I import get_data in another notebook/file, I don't also have to import
+# acquire_data or prepare_data
+def get_data():
+    data = acquire_data()
+    data = prepare_data(data)
+    return data
